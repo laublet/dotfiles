2677bd8f3b3c1c5108c8477f1c74ae1e000d080d
Loic Aublet <loic.aublet@blank.app>
2025-03-07 18:39:14 +0100

    update for mac

---
+++ first-install-mac.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Basics
+brew install bat fd fnm fzf git htop neovim ripgrep vim wget zoxide zsh 
+
+# Dev / work
+brew install awscli granted sqlite yarn 
+
+# Utils
+brew install bitwarden
+
+# Install Casks
+brew install --cask nikitabobko/tap/aerospace
+brew install --cask chatgpt copyq cursor choosy discord docker firefox firefox@developer-edition google-chrome obsidian postman raycast slack steermouse todoist vlc wezterm zoom
+
+brew install --cask aws-vpn-client
+
+# Perso
+brew install --cask nvidia-geforce-now philips-hue-sync
+
+# Install Fonts
+brew install --cask font-hack-nerd-font font-atkinson-hyperlegible
+
+# Need to go to the app store for these
+# amphetamine
+# prime tv
+# apple tv
+
